1c95475628f6dfb1caef76afadbe11e3df83cca4
mehan <mehan.alavi.88.majd@gmail.com>
2022-12-28 21:36:53 +0330

    Added api dir

---
+++ api/tweet.php
@@ -0,0 +1,23 @@
+<?php
+  $conn = new mysqli("localhost", "mehan", "mehan1388","login");
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_SESSION['username'];
+    $text = $_POST['text'];
+    $sql = "INSERT INTO tweet (username, text)
+      VALUES ('$username', '$text')";
+    
+    if ($conn->query($sql) === TRUE) {
+
+        ?> 
+        <?php
+        header("Location: /php-twitter/");
+        die();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+}
