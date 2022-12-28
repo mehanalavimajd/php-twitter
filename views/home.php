9b645c94e2eb8257ac95498704aec89d7200ff3a
mehan <mehan.alavi.88.majd@gmail.com>
2022-12-28 21:37:33 +0330

    Update home.php

--- views/home.php
+++ views/home.php
@@ -2,8 +2,16 @@
 session_start();
     if(isset($_SESSION['username'])){
         echo "Welcome " . $_SESSION['username'];
-    echo "<br>";
-    echo '<a href="/login/logout">Logout from here</a>';
+        echo "<br>";
+        echo '<a href="/php-twitter/logout">Logout from here</a>';
+        ?>
+        <form action="/php-twitter/api/tweet" method="post">
+            <input type="text" name="text">
+            <button type="submit">Tweet!</button>
+        </form>
+
+    <?php
+    
     }else{
         echo "You aren't logged in";
         echo "<br>";
