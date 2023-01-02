f03a092054b23b05b72cd557a5663575e7a822ad
mehan <mehan.alavi.88.majd@gmail.com>
2023-01-02 17:33:52 +0330

    added datetime and showing tweets in homepage

--- views/home.php
+++ views/home.php
@@ -1,4 +1,6 @@
 <?php
+    ini_set('error_reporting',E_ALL);
+    ini_set('display_errors',1);
 session_start();
     if(isset($_SESSION['username'])){
         echo "Welcome " . $_SESSION['username'];
@@ -17,4 +19,23 @@ session_start();
         echo "<br>";
         echo '<a href="/php-twitter/login">Login from here</a>';
     }
-?>
+    $conn = new mysqli("localhost", "mehan", "mehan1388","login");
+    $sql = "SELECT * FROM tweet ORDER BY date DESC LIMIT 0,25";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user = $row['username'];
+        $text = $row['text'];
+        $date = $row['date'];
+        echo "
+        <p style=\"opacity:40%\">$date</p>
+        <b> $user wrote:</b>
+        <p style=\"margin-left:40px\"> $text </p>
+        ";
+    }
+    } else {
+    echo "0 results";
+}
+    ?>
