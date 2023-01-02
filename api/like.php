c3c0830aa9bd45f4236e91ee4863548fd584573d
mehan <mehan.alavi.88.majd@gmail.com>
2023-01-02 19:48:39 +0330

    correcting redirect

--- api/like.php
+++ api/like.php
@@ -6,6 +6,11 @@
 $id = $_POST['id'];
 $user = $_SESSION['username'];
 $sql = "SELECT * FROM tweet where id=$id";
-if($conn->query($sql)===true){
-    
-}
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} 
--- views/logout.php
+++ views/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("location: /php-admin");
+header("location: /php-twitter");
 die();
 ?>
