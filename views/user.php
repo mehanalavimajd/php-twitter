8c58c47c9200393fbfe2f2c21070a1cfae54cf15
mehan <mehan.alavi.88.majd@gmail.com>
2023-05-21 20:50:36 +0330

    userpage bug when logout

--- views/user.php
+++ views/user.php
@@ -12,11 +12,16 @@ $result = $conn->query($sql_query);
 if ($result->num_rows == 1) {
   while ($row = $result->fetch_assoc()) {
 ?>
-
+<html>            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.1/css/all.css" crossorigin="anonymous">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+            <link rel="stylesheet" href="/php-twitter/public/style.css" media="screen"></html>
 <?php
     session_start();
     if (isset($_SESSION['username']))
     include("views/user-real.php");
+    else
+    echo "Login to follow and message";
 
     $conn = new mysqli("localhost", "mehan", "mehan1388", "login");
     $sql = "SELECT * FROM tweet where username='$user' ORDER BY date DESC LIMIT 0,25";
